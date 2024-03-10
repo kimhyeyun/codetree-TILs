22ead5d2bbe4fdaaa8415b8239c87d734181a1c3
HYEYUN <50236501+kimhyeyun@users.noreply.github.com>
2024-03-10 20:44:25 +0900

    Title: 1, 2, 3 더하기 2, Time: 126ms, Memory: 13MB, Status: Passed - CodeTree

--- 240310/1, 2, 3 더하기 2/1-2-3-plus-2.java
+++ 240310/1, 2, 3 더하기 2/1-2-3-plus-2.java
@@ -12,13 +12,21 @@ public class Main {
             return;
         }
 
+        if(n == 3) {
+            System.out.println(3);
+            return;
+        }
+
         dp[1][1] = 1;
         dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
 
-        for (int i = 3; i <= n; i++) {
+        for (int i = 4; i <= n; i++) {
             dp[i][1] = (dp[i - 1][2] + dp[i - 1][3]) % 1000000007;
             dp[i][2] = (dp[i - 2][1] + dp[i - 2][3]) % 1000000007;
-            dp[i][3] = (dp[i - 3][1] + dp[i - 3][1]) % 1000000007;
+            dp[i][3] = (dp[i - 3][1] + dp[i - 3][2]) % 1000000007;
         }
 
         long answer = (dp[n][1] + dp[n][2] + dp[n][3]) % 1000000007;
