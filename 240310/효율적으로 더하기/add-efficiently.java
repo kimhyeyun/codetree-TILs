d1220df33cd7184bb09118680449cb5210f98d86
HYEYUN <50236501+kimhyeyun@users.noreply.github.com>
2024-03-10 19:40:14 +0900

    Title: 효율적으로 더하기, Time: 3057ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

---
+++ 240310/효율적으로 더하기/add-efficiently.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int n, answer;
+    static int[] arrs;
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        arrs = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++){
+            arrs[i] = Integer.parseInt(st.nextToken());
+        }
+
+        answer = Integer.MAX_VALUE;
+        sol(0, new int[n], new boolean[n]);
+
+        System.out.println(answer);
+    }
+
+    static void sol(int count, int[] indexes, boolean[] isSelected) {
+        if (count == n) {
+            answer = Math.min(answer, cal(indexes));
+            return;
+        }
+
+        for (int i = 0; i < n ; i++) {
+            if (isSelected[i]) continue;
+
+            isSelected[i] = true;
+            indexes[count] = i;
+            sol(count + 1, indexes, isSelected);
+            isSelected[i] = false; 
+        }
+
+    }
+
+    static int cal (int[] indexes) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += (arrs[indexes[i]] * (n - i)); 
+        }
+
+        return sum;
+    }
+}
