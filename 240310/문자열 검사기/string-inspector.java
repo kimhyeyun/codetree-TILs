c0e659473ef5c12c5a8075dd57d4c26bfa6148b4
HYEYUN <50236501+kimhyeyun@users.noreply.github.com>
2024-03-10 20:28:43 +0900

    Title: 문자열 검사기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240310/문자열 검사기/string-inspector.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        Stack<Character> stack = new Stack<>();
+        for(char c : str.toCharArray()) {
+            if (c == '(' || c == '[') {
+                stack.add(c);
+            } else if (c == ')') {
+                if (stack.peek() != '(' || stack.isEmpty()) {
+                    System.out.println(0);
+                    return;
+                }
+                stack.pop();
+            } else if (c == ']') {
+                if (stack.peek() != '[' || stack.isEmpty()) {
+                    System.out.println(0);
+                    return;
+                }
+                stack.pop();
+            }
+        }
+
+        System.out.println(1);
+    }
+}
