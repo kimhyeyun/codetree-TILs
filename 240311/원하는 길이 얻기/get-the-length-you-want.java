0349e0c97b7805349b9fd914fcd0e93ce57a42d1
HYEYUN <50236501+kimhyeyun@users.noreply.github.com>
2024-03-11 16:40:31 +0900

    Title: 원하는 길이 얻기, Time: 259ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240311/원하는 길이 얻기/get-the-length-you-want.java
+++ 240311/원하는 길이 얻기/get-the-length-you-want.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 
+// 도저히 왜 959 인지 모르겠다...
 public class Main {
     public static void main(String[] args) throws Exception {
         // 여기에 코드를 작성해주세요.
@@ -30,11 +31,20 @@ public class Main {
         }
 
         list.sort((o1, o2) -> {
+            // 자른횟수 작고, 같다면 더 많은 조각 
             if (o1[1] == o2[1]) {
                 return o2[0] - o1[0];
             }
             return o1[1] - o2[1];
+
+            // if (o1[0] == o2[0]) {
+            //     return o1[1] - o2[1];
+            // }
+            // return o2[0] - o1[0];
         });
+        for (int[] arr : list) {
+            System.out.println(arr[0] + " " + arr[1]);
+        }
 
         for (int[] arr : list) {
             if (m == 0) break;
