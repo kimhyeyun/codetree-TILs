31df47270ee2311b48a65889a580d8c4d1d3d2d4
HYEYUN <50236501+kimhyeyun@users.noreply.github.com>
2024-03-20 19:10:54 +0900

    Title: 코드트리 오마카세, Time: 1418ms, Memory: 61MB, Status: Passed - CodeTree

--- 240320/코드트리 오마카세/codetree-omakase.java
+++ 240320/코드트리 오마카세/codetree-omakase.java
@@ -1,61 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
 	static int L, Q;
-	static Map<String, List<Integer>> sushis;
-	static Map<String, Customer> customerMap;
 
-	static class Command {
-		int time, command, position;
+	static class Query {
+		int cmd, t, x, n;
 		String name;
-		int count;
 
-		public Command(int command, int time) {
-			this.command = command;
-			this.time = time;
-		}
-
-		public Command(int time, int command, int position, String name) {
-			this.time = time;
-			this.command = command;
-			this.position = position;
+		public Query(int cmd, int t, int x, int n, String name) {
+			this.cmd = cmd;
+			this.t = t;
+			this.x = x;
+			this.n = n;
 			this.name = name;
-		}
-
-		public Command(int time, int command, int position, String name, int count) {
-			this.time = time;
-			this.command = command;
-			this.position = position;
-			this.name = name;
-			this.count = count;
 		}
 	}
 
-	static class Customer {
-		String name;
-		int count;
-		int position;
-		boolean isLeave;
-
-		public Customer(String name, int count, int position) {
-			this.name = name;
-			this.count = count;
-			this.position = position;
-			this.isLeave = false;
-		}
-
-		public void eat() {
-			this.count -= 1;
-		}
-	}
+	static List<Query> queries = new LinkedList<>();
+	static Set<String> customers = new HashSet<>();
+	static Map<String, List<Query>> queriesOfCustomer = new HashMap<>();
+	static Map<String, Integer> entryTimesOfCustomer = new HashMap<>();
+	static Map<String, Integer> positionOfCustomer = new HashMap<>();
+	static Map<String, Integer> leaveTimeOfCustomer = new HashMap<>();
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -64,129 +42,74 @@ public class Main{
 		L = Integer.parseInt(st.nextToken());
 		Q = Integer.parseInt(st.nextToken());
 
-		sushis = new HashMap<>();
-
-		List<Command> commandList = new LinkedList<>();
-		customerMap = new HashMap<>();
+		StringBuilder sb = new StringBuilder();
 
 		while (Q-- > 0) {
 			st = new StringTokenizer(br.readLine());
-
 			int command = Integer.parseInt(st.nextToken());
+			int t = -1, x = -1, n = -1;
+			String name = "";
+
 			if (command == 100) {
-				int t = Integer.parseInt(st.nextToken());
-				int x = Integer.parseInt(st.nextToken());
-				String name = st.nextToken();
+				t = Integer.parseInt(st.nextToken());
+				x = Integer.parseInt(st.nextToken());
+				name = st.nextToken();
 
-				commandList.add(new Command(t, command, x, name));
+				queriesOfCustomer.computeIfAbsent(name, k -> new ArrayList<>()).add(new Query(command, t, x, n, name));
 			} else if (command == 200) {
-				int t = Integer.parseInt(st.nextToken());
-				int x = Integer.parseInt(st.nextToken());
-				String name = st.nextToken();
-				int n = Integer.parseInt(st.nextToken());
+				t = Integer.parseInt(st.nextToken());
+				x = Integer.parseInt(st.nextToken());
+				name = st.nextToken();
+				n = Integer.parseInt(st.nextToken());
 
-				commandList.add(new Command(t, command, x, name, n));
+				customers.add(name);
+				entryTimesOfCustomer.put(name, t);
+				positionOfCustomer.put(name, x);
 			} else {
-				int t = Integer.parseInt(st.nextToken());
-
-				commandList.add(new Command(command, t));
+				t = Integer.parseInt(st.nextToken());
 			}
+
+			queries.add(new Query(command, t, x, n, name));
 		}
 
-		int currentTime = 0;
-		int index = 0;
+		for (String name : customers) {
+			leaveTimeOfCustomer.put(name, 0);
 
-		StringBuilder sb = new StringBuilder();
+			for (Query q : queriesOfCustomer.get(name)) {
+				int timeToRemoved = 0;
+				if (q.t < entryTimesOfCustomer.get(name)) {
+					int timeOfSushi = (q.x + (entryTimesOfCustomer.get(name) - q.t)) % L;
+					int additionalTime = (positionOfCustomer.get(name) - timeOfSushi + L) % L;
 
-		while (true) {
-			currentTime += 1;
-			rotateTable();
-
-			if (index >= commandList.size()) break;
-			if (currentTime == commandList.get(index).time) {
-				int command = commandList.get(index).command;
-
-				if (command == 100) {
-					int x = commandList.get(index).position;
-					String name = commandList.get(index).name;
-
-					List<Integer> list = new LinkedList<>();
-					if (sushis.containsKey(name)) list = sushis.get(name);
-
-					list.add(x);
-					sushis.put(name, list);
-
-					eat();
-				} else if (command == 200) {
-					int x = commandList.get(index).position;
-					String name = commandList.get(index).name;
-					int count = commandList.get(index).count;
-
-					Customer c = new Customer(name, count, x);
-					customerMap.put(name, c);
-
-					eat();
+					timeToRemoved = entryTimesOfCustomer.get(name) + additionalTime;
 				} else {
-					eat();
-
-					int cCount = 0, sCount = 0;
-					for (String name : customerMap.keySet()) {
-						if (!customerMap.get(name).isLeave)
-							cCount += 1;
-					}
-
-					for (String name : sushis.keySet()) {
-						sCount += sushis.get(name).size();
-					}
-
-					sb.append(cCount).append(" ").append(sCount).append("\n");
+					int additionalTime = (positionOfCustomer.get(name) - q.x + L) % L;
+					timeToRemoved = q.t + additionalTime;
 				}
-				index += 1;
+
+				leaveTimeOfCustomer.put(name, Math.max(leaveTimeOfCustomer.get(name), timeToRemoved));
+				queries.add(new Query(111, timeToRemoved, -1, -1, name));
 			}
 		}
+
+		for (String name : customers) {
+			queries.add(new Query(222, leaveTimeOfCustomer.get(name), -1, -1, name));
+		}
+
+		queries.sort((o1, o2) -> {
+			if (o1.t == o2.t) return o1.cmd - o2.cmd;
+			return o1.t - o2.t;
+		});
+
+		int customerCount = 0, sushiCount = 0;
+		for (Query query : queries) {
+			if (query.cmd == 100) sushiCount += 1;
+			else if (query.cmd == 111) sushiCount -= 1;
+			else if (query.cmd == 200) customerCount += 1;
+			else if (query.cmd == 222) customerCount -= 1;
+			else sb.append(customerCount).append(" ").append(sushiCount).append("\n");
+		}
+
 		System.out.println(sb);
-	}
-
-	private static void eat() {
-		if (customerMap.isEmpty()) return;
-
-		List<String> names = new LinkedList<>();
-		for (String name : customerMap.keySet()) {
-			if (!sushis.containsKey(name)) continue;
-
-			Customer c = customerMap.get(name);
-			int x = c.position;
-
-			List<Integer> list = sushis.get(name);
-			for (int i = 0; i < list.size(); i++) {
-				if (c.count == 0) break;
-				if (list.get(i) == x) {
-					c.eat();
-					list.remove(i);
-					i -= 1;
-				}
-			}
-
-			if (c.count <= 0) names.add(name);
-			else customerMap.put(name, c);
-
-			sushis.put(name, list);
-		}
-
-		for (String n : names) {
-			customerMap.remove(n);
-		}
-	}
-
-	private static void rotateTable() {
-		for (String name : sushis.keySet()) {
-			List<Integer> tmp = new LinkedList<>();
-
-			for (int i = 0; i < sushis.get(name).size(); i++) {
-				tmp.add((sushis.get(name).get(i) + 1) % L);
-			}
-
-			sushis.put(name, tmp);
-		}
 	}
 }
